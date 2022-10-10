4d9baaf277edd273f657942ae1a5e3adbf6880d5
MuhammedMGS <70213359+muhammedbfm@users.noreply.github.com>
2022-10-10 15:22:14 +0300

    CMTL ue1

---
+++ mfe-project/mfe-project/menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mfe_project
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
